d0fd625573b6b5402901760c6b62bdda4abd9783
“G33ktech” <cv3tech@gmail.com>
2022-05-22 00:38:23 +0700

    init.config.files.010 add src/*.

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div id="nav" style="margin:auto">
+      <router-link to="/AppView"><i class="material-icons">home</i></router-link> 
+      <router-link to="/Chat"><i class="material-icons">playlist_add_check</i></router-link> 
+      <router-link to="/GrabOrder"><i class="material-icons">swipe</i></router-link> 
+      <router-link to="/DepositView"><i class="material-icons">chat</i></router-link> 
+      <router-link to="/WithdrawView"><i class="material-icons">person</i></router-link>
+      <router-link to="/"><i class="material-icons">logout</i></router-link> 
+    </div>
+    <router-view />
+  </div>
+</template>
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    background: linear-gradient(to bottom, #33ccff 0%, #3366ff 50%);
+    padding: 5px;
+    margin: -5px;
+    position: relative;
+  }
+  #nav {
+    padding: 30px;
+    width: 500px;
+    position: fixed;
+    bottom: 0;
+    background-color: purple;
+    a {
+      font-weight: bold;
+      color: lightgray;
+      &.router-link-exact-active {
+        color: white;
+      }
+    }
+  }
+</style>
